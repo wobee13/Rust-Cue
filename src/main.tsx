1668dd3d9c0e6463d9478da11bc1a0dc6015d832
Shelby VandenHoek <wobee13@gmail.com>
2021-11-26 09:10:16 -0600

    Added Wouter and Dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import { ColorModeScript } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import theme from './theme/theme'
 
 ReactDOM.render(
   <React.StrictMode>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
